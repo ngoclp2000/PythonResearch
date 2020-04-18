5e0f5a2ff70e44b58306c8f3e7de926febe08418
ngoclp2000 <nindaphi@gmail.com>
2020-04-18 23:18:34 +0700

    asd

--- Week3/main.py
+++ Week3/main.py
@@ -1,6 +1,7 @@
 import os
 from collections import Counter
 import pandas as pd
+import matplotlib.pyplot as plt
 def countWords(text):
     text = text.lower()
     skips = [",",".",";",":","'",'"']
@@ -36,4 +37,18 @@ for language in os.listdir(book_dir):
             (num_unique, counts) = wordStats(countWords(text))
             stats.loc[title_num] = language , author, title , sum(counts) , num_unique
             title_num += 1
-print(stats.length)    
+# plt.loglog(stats.length, stats.unique,"bo")
+# plt.show()
+plt.figure(figsize = (10,10))
+subset = stats[stats.language == "English"]
+plt.loglog(subset.length, subset.unique,"o", label ="English" , color ="crimson")
+subset = stats[stats.language == "French"]
+plt.loglog(subset.length, subset.unique,"o", label ="English" , color ="forestgreen")
+subset = stats[stats.language == "German"]
+plt.loglog(subset.length, subset.unique,"o", label ="English" , color ="orange")
+subset = stats[stats.language == "Portuguese"]
+plt.loglog(subset.length, subset.unique,"o", label ="English" , color ="blueviolet")
+plt.legend()
+plt.xlabel("Book length")
+plt.ylabel("Number of unique words")
+plt.savefig("lang_plot.pdf")
