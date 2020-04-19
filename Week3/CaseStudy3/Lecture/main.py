8ae96dec4c1a9bb7cd25b37d11cc37c51ce0d5b0
ngoclp2000 <nindaphi@gmail.com>
2020-04-20 01:21:57 +0700

    asd

--- Week3/CaseStudy3/Lecture/main.py
+++ Week3/CaseStudy3/Lecture/main.py
@@ -23,3 +23,30 @@ points = np.array([[1,1],[1,2],[1,3],[2,1],[2,2],[2,3],[3,1],[3,2],[3,3]])
 p = np.array([2,2.5])
 outcome = np.array([0,0,0,0,1,1,1,1,1])
 print(type(points.shape))
+
+def generate_synth_data(n=50):
+    """ Create two sets of points from bivariate normal distributions. """
+    points = np.concatenate((ss.norm(0,1).rvs((n,2)) , ss.norm(1,1).rvs((n,2))),axis= 0)
+    outcomes = np.concatenate((np.repeat(0,n),np.repeat(1,n)))
+    return (points,outcomes)
+n = 20
+points, outcomes = generate_synth_data(n)
+plt.figure()
+plt.plot(points[:n,0] , points[:n,1] , "ro")
+plt.plot(points[n:,0] , points[n:,1], "bo")
+plt.savefig("bivariate.pdf")
+
+def make_prediction_grid(predictors,outcomes,limits,h,k):
+    """ Classify each point on the prediction grid. """
+    (x_min, x_max , y_max , y_min) = limits
+    xs = np.arange(x_min, x_max , h)
+    ys = np.arange(y_min, y_max, h)
+    xx , yy = np.meshgrid(xs, ys)
+
+    prediction_grid = np.zeros(xx.shape, dtype= int )
+    for i,x in enumerate(xs):
+        for j,y in enumerate(ys):
+            p = np.array([x,y])
+            prediction_grid[j,i] = knn_predict(p, predictors, outcomes, k)
+    
+    return (xx, yy, prediction_grid)
